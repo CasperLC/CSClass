b14f7d61fd5133f3d0cc16b893fa8b5ab8b93fb4
Neller1989 <43983250+Neller1989@users.noreply.github.com>
2018-11-21 11:29:31 +0100

    gui works
    back and enter work

---
+++ MovieRecommendationSystem1-master/src/movierecsys/gui/controller/MovieOptionRecViewController.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.gui.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Bruger
+ */
+public class MovieOptionRecViewController implements Initializable
+{
+
+    @FXML
+    private AnchorPane MovieBorderPane;
+    @FXML
+    private Button btnXMovieBack;
+    @FXML
+    private Button btnAddMovieOption;
+    @FXML
+    private Button btnRemoveMovieOption;
+    @FXML
+    private TextField txtMovieAdd;
+    @FXML
+    private TextField txtMovieYear;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+
+    @FXML
+    private void xMovieBack(ActionEvent event) throws IOException
+    {
+        Stage stage = (Stage) MovieBorderPane.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/movierecsys/gui/view/MovieRecView.fxml"));
+        Scene scene = new Scene (root);
+        stage.setScene (scene);
+    }
+
+    @FXML
+    private void btnaddMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnRemoveMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void movieAddText(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void movieYearText(ActionEvent event)
+    {
+    }
+    
+}
